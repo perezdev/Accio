eeca51e9a87b1adce99ac3ba02ee2a06c24a00e1
perezdev <ernest.perezjr@gmail.com>
2020-03-06 15:49:54 -0600

    Updating some files to support accio css/js

--- HpTcgCardBrowser.Web/wwwroot/js/site.js
+++ HpTcgCardBrowser.Web/wwwroot/js/site.js
@@ -1,16 +1,1 @@
-﻿function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-function until(conditionFunction) {
-    const poll = resolve => {
-        if (conditionFunction()) resolve();
-        else setTimeout(_ => poll(resolve), 400);
-    };
-    return new Promise(poll);
-}
+﻿
