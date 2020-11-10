dfe06bb6aa77f0394e13ce24b787849cfd9c5d69
perezdev <ernest.perezjr@gmail.com>
2020-11-10 17:39:41 -0600

    Added initial changes for login system

--- Accio.Presentation.Web/wwwroot/js/accio.account.login.js
+++ Accio.Presentation.Web/wwwroot/js/accio.account.login.js
@@ -1,1 +1,40 @@
-﻿
+﻿const loginElements = {
+    LoginButtonId: '#login',
+    EmailTextId: '#emailAddress',
+    PasswordTextId: '#password',
+};
+
+$(document).ready(function () {
+    InitializeElements();
+});
+
+function InitializeElements() {
+    $(loginElements.LoginButtonId).click(function (e) {
+        e.preventDefault();
+        Login();
+    });
+}
+
+function Login() {
+    ClearLoginErrors();
+
+    var emailAddress = $(loginElements.EmailTextId).val();
+    var password = $(loginElements.PasswordTextId).val();
+
+    if (!emailAddress || !password) {
+        ShowLoginErrors('Email address or password is empty.');
+        return;
+    }
+
+    alert('test');
+}
+
+function ShowLoginErrors(message) {
+    $(registerElements.ErrorId).html('');
+    $(registerElements.ErrorId).html(message);
+    $(registerElements.ErrorId).show();
+}
+function ClearLoginErrors() {
+    $(registerElements.ErrorId).html('');
+    $(registerElements.ErrorId).hide();
+}
