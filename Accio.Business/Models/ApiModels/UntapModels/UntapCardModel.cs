f7c1b4076cec8ea62f841f4ed5b445cee8ac5efa
perezdev <ernest.perezjr@gmail.com>
2021-05-04 20:09:46 -0500

    Added API support to web project. Added untap card end point for untap integration

---
+++ Accio.Business/Models/ApiModels/UntapModels/UntapCardModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Accio.Business.Models.ApiModels.UntapModels
+{
+    public class UntapCardModel
+    {
+        public string Name { get; set; }
+        public string SetCode { get; set; }
+        public string Text { get; set; }
+        public string ImageUrl { get; set; }
+        public string Orientation { get; set; }
+        public string Rarity { get; set; }
+    }
+}
