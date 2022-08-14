7e35c9ba9ff089976cf8be1b33920f06000415dc
Ernest Perez Jr <ernest.perezjr@clubcorp.com>
2022-08-13 19:38:07 -0500

    Updated API project for some testing Updated untap end points for logging purposes

--- Accio.Presentation.Web.API/Startup.cs
+++ Accio.Presentation.Web.API/Startup.cs
@@ -3,6 +3,21 @@ using Accio.Business.Services.CardServices;
 using Accio.Business.Services.LanguageServices;
 using Accio.Business.Services.LessonServices;
 using Accio.Business.Services.SourceServices;
+using Accio.Business.Services.AccountServices;
+using Accio.Business.Services.AdvancedCardSearchSearchServices;
+using Accio.Business.Services.ApiServices.UntapServices;
+using Accio.Business.Services.AuthenticationHistoryServices;
+using Accio.Business.Services.AuthenticationServices;
+using Accio.Business.Services.ConfigurationServices;
+using Accio.Business.Services.EmailServices;
+using Accio.Business.Services.FormatServices;
+using Accio.Business.Services.ImageServices;
+using Accio.Business.Services.RoleServices;
+using Accio.Business.Services.RulingRestrictionServices;
+using Accio.Business.Services.RulingServices;
+using Accio.Business.Services.TypeServices;
+using Accio.Business.Services.AccountRoleServices;
+
 using Accio.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -38,6 +53,33 @@ namespace Accio.Presentation.Web.API
             services.AddTransient<LessonService>();
             services.AddTransient<CardSearchHistoryService>();
             services.AddTransient<SourceService>();
+            services.AddTransient<CardRulingService>();
+            services.AddTransient<RulingService>();
+            services.AddTransient<RulingSourceService>();
+            services.AddTransient<RulingTypeService>();
+            services.AddTransient<ConfigurationService>();
+            services.AddTransient<CardSubTypeService>();
+            services.AddTransient<SubTypeService>();
+            services.AddTransient<CardProvidesLessonService>();
+            services.AddTransient<AdvancedCardSearchService>();
+            services.AddTransient<CardImageService>();
+            services.AddTransient<ImageService>();
+            services.AddTransient<ImageSizeService>();
+            services.AddTransient<SingleCardService>();
+            services.AddTransient<AccountService>();
+            services.AddTransient<AuthenticationService>();
+            services.AddTransient<AuthenticationHistoryService>();
+            services.AddTransient<ConfigurationService>();
+            services.AddTransient<EmailService>();
+            services.AddTransient<AccountVerificationNumberService>();
+            services.AddTransient<AccountVerificationService>();
+            services.AddTransient<AuthenticationService>();
+            services.AddTransient<AccountRoleService>();
+            services.AddTransient<RoleService>();
+            services.AddTransient<RulingRestrictionService>();
+            services.AddTransient<CardRulingRestrictionService>();
+            services.AddTransient<FormatService>();
+            services.AddTransient<UntapService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
